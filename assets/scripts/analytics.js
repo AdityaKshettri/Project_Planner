b3435dee1ddc79b67e63eab8924dc9d8814b59fd
Aditya Kshettri <36606431+AdityaKshettri@users.noreply.github.com>
2020-01-26 22:10:40 +0530

    Delete analytics.js

--- assets/scripts/analytics.js
+++
@@ -1,7 +0,0 @@
-const intervalId = setInterval(() => {
-    console.log("Starting Analytics...")
-},2000);
-
-document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-    clearInterval(intervalId);
-});
